6d97057fa71cffaa86911d68ed546892798dd029
Max <89087395+Stark1ll@users.noreply.github.com>
2022-05-26 10:53:12 +0300

    Delete src/firebase directory

--- src/firebase/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("E:\CODING\ReactAPP\restaurant-site\pigga-29297-firebase-adminsdk-2cosl-79c515216c.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://pigga-29297-default-rtdb.europe-west1.firebasedatabase.app"
-});
